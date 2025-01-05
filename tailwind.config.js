0ef3de5c221fb2002ac61b4003ce73dce73e6024
Arush <arush.universe@gmail.com>
2025-01-05 23:25:20 +0530

    Landing Page Done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    mode: "jit",
+    purge: ["index.html"],
+    theme: {
+        extend: {
+            height: {
+                '500': '500px', // Define a custom height
+            },
+        },
+    },
+  };
